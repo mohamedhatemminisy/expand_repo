9d4aa51dca95236c6ea1ef21940c4510f96b1c70
mohamedhatemminisy <mohamed.hatem100100@gmail.com>
2021-09-19 20:40:26 +0200

    test

--- resources/views/dashboard/archive/outArchive.blade.php
+++ resources/views/dashboard/archive/outArchive.blade.php
@@ -177,6 +177,8 @@
                                         <div class="row attachs-body">
                                             <div class="form-group col-12 mb-2">
                                                 <input type="hidden" name="fromname" value="formDataaa">
+                                                <meta name="csrf-token" content="{{ csrf_token() }}" />
+
                                                 <input type="file" class="form-control-file" id="formDataaaupload-file[]" multiple="" name="formDataaaUploadFile[]" onchange="doUploadAttach('formDataaa')" 
                                                 style="display: none" >
                                                 <div class="row formDataaaFilesArea" style="margin-left: 0px;">
@@ -388,5 +390,93 @@ $( function() {
                             +'</div>')
 
     }
+    function doUploadAttach(formDataStr){
+        $.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+    $(".loader").removeClass('hide');
+    $(".form-actions").addClass('hide');
+    var formData = new FormData($("#"+formDataStr)[0]);
+    $.ajax({
+        url: 'uploadAttach',
+        type: 'POST',
+        data: formData,
+        dataType:"json",
+        async: true,
+        success: function (data) {
+            row='';
+            row1='';
+            if(data.status.success){
+                for(j=imgCounter;j<data.img.length;j++){
+        if(data.img[j].type==1 ) {
+            shortCutName=data.img[j].orgname;
+                        shortCutName=shortCutName.substring(0, 40)
+                        row+='<div id="attach" class=" col-sm-6 ">' +
+                            '   <div class="attach" onmouseover="$(this).children().first().next().show()">'
+                            +'    <span class="attach-text">'+shortCutName+'</span>'
+                            +'    <a class="attach-close1" href="'+realPath+'uploads/'+data.img[j].name+'" style="color: #74798D; float:left;" target="_blank"><i class="fa fa-eye"></i></a>'
+                            +'    <a class="attach-close1" style="color: #74798D; float:left;" onclick="$(this).parent().parent().remove()">×</a>'
+                            +'      <input type="hidden" id="'+formDataStr+'imgUploads[]" name="'+formDataStr+'imgUploads[]" value="'+data.img[j].name+'">'
+                            +'             <input type="hidden" id="'+formDataStr+'orgNameList[]" name="'+formDataStr+'orgNameList[]" value="'+data.img[j].orgname+'">'
+                            +'    </div>'
+                            +'  </div>' +
+                             '</div>'
+        }
+                    if(data.img[j].type==2 ) {
+          row1+='<div  class="col-sm-2" id="i'+(j+1)+'">'
+            +'     <div class="row"  onmouseover="$(this).children().first().children().first().next().show()"  onmouseout="$(this).children().first().children().first().next().hide()">'
+                            +'         <div class="col-sm-12" style="height: 80px;">'
+                            +'         <a class="group1" href="'+realPath+'uploads/'+data.img[j].name+'" title="'+data.img[j].orgname+'" style="color: #74798D" >' +
+                            '                <img src="'+realPath+'uploads/'+data.img[j].name+'" title="'+data.img[j].orgname+'" id="imgSlider'+(j+1)+'" style="max-height:80px;"/></a>'
+
+                            +'           <a class="attach-close" style="color: #74798D" onclick="$(this).parent().parent().parent().remove()" ><i class="fa fa-times"></i></a>'
+                            +'             <input type="hidden" id="'+formDataStr+'imgUploads[]" name="'+formDataStr+'imgUploads[]" value="'+data.img[j].name+'">'
+                            +'             <input type="hidden" id="'+formDataStr+'orgNameList[]" name="'+formDataStr+'orgNameList[]" value="'+data.img[j].orgname+'">'
+                            +'         </div>'
+            +'     </div>'
+            +'</div>'
+                    }
+                }
+                //$(".attachs-carousel-container").html(row)
+                $(".alert-danger").addClass("hide");
+                $(".alert-success").removeClass('hide');
+                $("#succMsg").text(data.status.msg)
+      $("."+formDataStr+"FilesArea").append(row)
+      $("."+formDataStr+"ImagesArea").append(row1)
+                $(".loader").addClass('hide');
+                document.getElementById(""+formDataStr+"upload-file[]").value="";
+                document.getElementById(""+formDataStr+"upload-image[]").value="";
+
+                $(".group1").colorbox({rel:'group1'});
+                setTimeout(function(){
+                    $(".alert-danger").addClass("hide");
+                    $(".alert-success").addClass("hide");
+                },2000)
+            }
+            else {
+                $(".alert-success").addClass("hide");
+                $(".alert-danger").removeClass('hide');
+                $("#errMsg").text(data.status.msg)
+            }
+            $(".loader").addClass('hide');
+            $(".form-actions").removeClass('hide');
+        },
+        error:function(){
+            $(".alert-success").addClass("hide");
+            $(".alert-danger").removeClass('hide');
+            $("#errMsg").text(data.status.msg)
+            $(".loader").addClass('hide');
+            $(".form-actions").removeClass('hide');
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+}
+  
+
 </script>
 @endsection
